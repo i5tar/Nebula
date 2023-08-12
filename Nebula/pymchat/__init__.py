07f09d3e26318b5332fc0018b552f4f8497c11c8
i5tar <118033683+i5tar@users.noreply.github.com>
2023-08-11 21:26:05 -0600

    Delete Nebula directory

--- Nebula/pymchat/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# Created at 30/03/2021
-__license__ = "GNU General Public License v3.0"
-__author__ = "Alexandre Silva // MrKelpy"
-__copyright__ = "© Alexandre Silva 2021"
-__version__ = 1.0
